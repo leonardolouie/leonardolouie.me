2aeb08f9bf176cacc187269d71e71cdd8440a555
leonardolouie <leonardolouie30@gmail.com>
2019-11-02 01:01:34 +0800

    preloading

--- web/gatsby-browser.js
+++ web/gatsby-browser.js
@@ -5,6 +5,3 @@
  */
 
 // You can delete this file if you're not using it
-import '@fortawesome/fontawesome-free/css/all.min.css'
-import 'bootstrap-css-only/css/bootstrap.min.css'
-import 'mdbreact/dist/css/mdb.css'
--- web/src/components/header/index.js
+++ web/src/components/header/index.js
@@ -1,4 +1,8 @@
 import React, { useState } from 'react'
+import '@fortawesome/fontawesome-free/css/all.min.css'
+import 'bootstrap-css-only/css/bootstrap.min.css'
+import 'mdbreact/dist/css/mdb.css'
+
 import './header.scss'
 
 const Header = () => {
