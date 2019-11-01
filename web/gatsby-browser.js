cef6ef46bc3c763883308847f0d83c95a1e6f040
leonardolouie <leonardolouie30@gmail.com>
2019-11-02 01:11:05 +0800

    boss

--- web/gatsby-browser.js
+++ web/gatsby-browser.js
@@ -5,3 +5,6 @@
  */
 
 // You can delete this file if you're not using it
+import '@fortawesome/fontawesome-free/css/all.min.css'
+import 'bootstrap-css-only/css/bootstrap.min.css'
+import 'mdbreact/dist/css/mdb.css'
