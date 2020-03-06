ea4a158db1f2ef49e237c11354f33470e1278595
Leonardo Louie Ordonez <leonardolouie30@gmail.com>
2020-03-06 23:54:37 +0800

    WIP CHAT BOT

--- web/src/components/layout/index.js
+++ web/src/components/layout/index.js
@@ -3,12 +3,13 @@ import Navbar from '../navbar'
 import Footer from '../footer'
 import PropTypes from 'prop-types'
 import SiteHelmet from './siteHelmet'
-
+import SiteMessenger from './siteMessenger'
 export default function Layout(props) {
   const { children, icon } = props
   return (
     <>
       <SiteHelmet {...props} />
+      <SiteMessenger/>
       <Navbar icon={icon} />
       {children}
       <Footer />
---
+++ web/src/components/layout/siteMessenger/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Helmet from 'react-helmet'
+
+
+const SiteMessenger = () => {
+ 
+  return (
+    <Helmet>
+   
+    </Helmet>
+  )
+}
+
+export default SiteMessenger
