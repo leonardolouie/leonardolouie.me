40d17134e009c94b125c26b7196f39794add8fc0
Leonardo Louie Ordonez <leonardolouie30@gmail.com>
2020-03-07 00:01:51 +0800

    added bot

--- web/src/components/layout/siteMessenger/index.js
+++ web/src/components/layout/siteMessenger/index.js
@@ -6,7 +6,7 @@ const SiteMessenger = () => {
  
   return (
     <Helmet>
-   
+      <script src="https://app.rocketbots.io/facebook/chat/plugin/23992/463799947162187" async></script>
     </Helmet>
   )
 }
